76de41662b13450543e3bf7a8b650b90c1963a63
Bruno Pereira <bruno9pereira@gmail.com>
2016-04-03 22:02:24 -0300

    Update startup.sh

--- rails/startup.sh
+++ rails/startup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-if [ -e Gemfile ] then
-service mysql start
-bundle install
-rails server -b 0.0.0.0
+if [-e Gemfile]
+then
+  service mysql start
+  bundle install
+  rails server -b 0.0.0.0
 fi
 
 /bin/bash
